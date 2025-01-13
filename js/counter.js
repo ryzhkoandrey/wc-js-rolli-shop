aada82bd9e8efbcc80563f538cbf81a05565a186
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-13 02:50:11 +0200

    +

--- js/counter.js
+++ js/counter.js
@@ -1,3 +1,11 @@
 const btnMinus = document.querySelector('[data-action="minus"]');
 const btnPlus = document.querySelector('[data-action="plus"]');
+const counter = document.querySelector('[data-counter]');
 
+btnMinus.addEventListener('click', function () {
+    counter.innerText = --counter.innerText;
+});
+
+btnPlus.addEventListener('click', function () {
+    counter.innerText = ++counter.innerText;
+});
