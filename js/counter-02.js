4aca44266f17733301c7876da27516ef4d911fe2
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-14 03:55:30 +0200

    +

---
+++ js/counter-02.js
@@ -0,0 +1,12 @@
+// Добавляем прослушку на всем окне
+window.addEventListener('click', function (event) {
+    // Проверяем является ли элемент по которому был совершен клик кнопкой Плюс
+    if (event.target.dataset.action === 'plus') {
+
+    }
+
+    // Проверяем является ли элемент по которому был совершен клик кнопкой Минус
+    if (event.target.dataset.action === 'minus') {
+
+    }
+});
