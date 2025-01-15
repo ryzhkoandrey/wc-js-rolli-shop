d639078eadda7ae95b46aa3fff2a557c802f0cf8
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-15 05:22:36 +0200

    +

--- js/counter-02.js
+++ js/counter-02.js
@@ -1,12 +1,26 @@
 // Добавляем прослушку на всем окне
 window.addEventListener('click', function (event) {
+
+    let counter;
+
+    // Проверяем клик строго по кнопкам Плюс либо Минус
+    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+        // Находим обертку счетчика
+        const counterWrapper = event.target.closest('.counter-wrapper');
+        // Находим div с числом счетчика
+        counter = counterWrapper.querySelector('[data-counter]');
+    }
+
     // Проверяем является ли элемент по которому был совершен клик кнопкой Плюс
     if (event.target.dataset.action === 'plus') {
-
+        counter.innerText = ++counter.innerText;
     }
 
     // Проверяем является ли элемент по которому был совершен клик кнопкой Минус
     if (event.target.dataset.action === 'minus') {
 
+        if (counter.innerText > 1) {
+            counter.innerText = --counter.innerText;
+        }
     }
 });
