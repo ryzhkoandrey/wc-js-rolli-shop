77b52a83060295ffd3b17740d6bd0223a91f94c3
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-15 21:47:23 +0200

    Update cart-01.js

--- js/cart-01.js
+++ js/cart-01.js
@@ -22,29 +22,32 @@ window.addEventListener('click', function (event) {
         }
 
         // Собранные данные подставим в шаблон для товара в корзине
-        const cartItemHTML = `<div class="cart-item" data-id="02">
-								<div class="cart-item__top">
-									<div class="cart-item__img">
-										<img src="${productInfo.imgSrc}" alt="">
-									</div>
-									<div class="cart-item__desc">
-										<div class="cart-item__title">${productInfo.title}</div>
-										<div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>
+        const cartItemHTML = `
+            <!-- Cart item -->
+            <div class="cart-item" data-id="${productInfo.id}">
+				<div class="cart-item__top">
+					<div class="cart-item__img">
+						<img src="${productInfo.imgSrc}" alt="${productInfo.title}">
+					</div>
+					<div class="cart-item__desc">
+						<div class="cart-item__title">${productInfo.title}</div>
+						<div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>
 
-										<!-- cart-item__details -->
-										<div class="cart-item__details">
-											<div class="items items--small counter-wrapper">
-												<div class="items__control" data-action="minus">-</div>
-												<div class="items__current" data-counter="">${productInfo.counter}</div>
-												<div class="items__control" data-action="plus">+</div>
-											</div>
-											<div class="price">
-												<div class="price__currency">${productInfo.price}</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>`;
+						<!-- cart-item__details -->
+						<div class="cart-item__details">
+							<div class="items items--small counter-wrapper">
+								<div class="items__control" data-action="minus">-</div>
+								<div class="items__current" data-counter="">${productInfo.counter}</div>
+								<div class="items__control" data-action="plus">+</div>
+							</div>
+							<div class="price">
+								<div class="price__currency">${productInfo.price}</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+        `;
 
         // Отобразим товар в корзине
         cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
