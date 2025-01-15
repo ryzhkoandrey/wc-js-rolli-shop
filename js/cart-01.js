8fc5a7f05beedf5b68b81ed0d2c6d4faef5423c4
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-15 21:13:14 +0200

    +

--- js/cart-01.js
+++ js/cart-01.js
@@ -12,10 +12,37 @@ window.addEventListener('click', function (event) {
             id: card.dataset.id,
             imgSrc: card.querySelector('.product-img').getAttribute('src'),
             title: card.querySelector('.item-title').innerText,
-            itemsInBox: card.querySelector('[data-items-in-box]'),
+            itemsInBox: card.querySelector('[data-items-in-box]').innerText,
             weight: card.querySelector('.price__weight').innerText,
             price: card.querySelector('.price__currency').innerText,
             counter: card.querySelector('[data-counter]').innerText,
         }
+
+        // Собранные данные подставим в шаблон для товара в корзине
+        const cartItemHTML = `<div class="cart-item" data-id="02">
+								<div class="cart-item__top">
+									<div class="cart-item__img">
+										<img src="${productInfo.imgSrc}" alt="">
+									</div>
+									<div class="cart-item__desc">
+										<div class="cart-item__title">${productInfo.title}</div>
+										<div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>
+
+										<!-- cart-item__details -->
+										<div class="cart-item__details">
+											<div class="items items--small counter-wrapper">
+												<div class="items__control" data-action="minus">-</div>
+												<div class="items__current" data-counter="">${productInfo.counter}</div>
+												<div class="items__control" data-action="plus">+</div>
+											</div>
+											<div class="price">
+												<div class="price__currency">${productInfo.price}</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>`;
+
+        // Отобразим товар в корзине
     }
 });
