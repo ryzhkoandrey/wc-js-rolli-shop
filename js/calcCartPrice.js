c7a36e108ee9b5855934c23306984f9409f98035
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-18 03:09:16 +0200

    Update calcCartPrice.js

--- js/calcCartPrice.js
+++ js/calcCartPrice.js
@@ -1,16 +1,20 @@
 function calcCartPrice() {
 
-    const cartItems = document.querySelectorAll('.cart-item');
-    let totalPrice = 0;
+    const cartWrapper = document.querySelector('.cart-wrapper');
+    const priceElements = cartWrapper.querySelectorAll('.price__currency');
 
-    cartItems.forEach(function (item) {
-        const amountEl = item.querySelector('[data-counter]');
-        const priceEl = item.querySelector('.price__currency');
+    // Общая стоимость товаров
+    let priceTotal = 0;
 
-        const currentPrice = parseInt(amountEl.innerText) * parseInt(priceEl.innerText);
+    // Обходим все блоки с ценами в корзине
+    priceElements.forEach(function (item) {
 
-        totalPrice += currentPrice;
+        // Находим количество товара
+        const amountEl = item.closest('.cart-item').querySelector('[data-counter]');
+
+        // Добавляем стоимость товара в общую стоимость (количество * цену)
+        priceTotal += parseInt(item.innerText) * parseInt(amountEl.innerText);
     });
 
-    console.log(totalPrice);
+    console.log(priceTotal);
 }
